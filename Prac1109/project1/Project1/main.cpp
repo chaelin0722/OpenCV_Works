d8d90e818cf9865f73415f4901888d46483cca15
김채린 <ccaa9697@gmail.com>
2020-11-11 14:28:29 +0900

    hough polygon for문으로 자동 돌아가게 코드 변경

--- Prac1109/project1/Project1/main.cpp
+++ Prac1109/project1/Project1/main.cpp
@@ -8,6 +8,8 @@
 
 using namespace cv;
 using namespace std;
+//practice.exe ../../../../images/binaryGroup.bmp ../../../../images/group.jpg ../../../../images/group.jpg 
+// 위와 같이이미지 세개 argv 에서 받아야 함
 
 int main(int argc, char** argv){
 	Mat image;
@@ -17,14 +19,16 @@ int main(int argc, char** argv){
 	namedWindow("Binary Image");
 	imshow("Binary Image", image);
 	// Get the contours of the connected components
-	vector<std::vector<cv::Point>> contours;
-	findContours(image,	contours, RETR_EXTERNAL,CHAIN_APPROX_NONE); // retrieve all pixels of each
+	vector<vector<Point>> contours;
+	findContours(image,	contours, RETR_EXTERNAL,CHAIN_APPROX_NONE); //컨투어 검출
 
-	Mat result(image.size(), CV_8U, cv::Scalar(255));
-	drawContours(result, contours, -1, Scalar(0), 2); // with a thickness of 2
-	cv::namedWindow("Contours");
-	cv::imshow("Contours", result);
-	// Eliminate too short or too long contours
+	Mat result(image.size(), CV_8U, Scalar(255));
+	drawContours(result, contours, -1, Scalar(0), 2); // with a thickness of 2 컨투어 검출된거 그리기
+
+	namedWindow("Contours");
+	imshow("Contours", result);
+	
+	//min max 설정해서 지우기
 	int cmin = 100; // minimum contour length
 	int cmax = 1000; // maximum contour length
 
@@ -36,57 +40,56 @@ int main(int argc, char** argv){
 			++itc;
 	}
 	// draw contours on the original image
-	Mat original = cv::imread(argv[2], 1);
+	Mat original = imread(argv[2], 1);
 	drawContours(original, contours,-1,Scalar(255, 255, 255), 2); // with a thickness of 2
-	cv::namedWindow("Contours on Animals");
-	cv::imshow("Contours on Animals", original);
+	namedWindow("Contours on Animals");
+	imshow("Contours on Animals", original);
 
-//<< 새로운 descriptor 삽입 >> (다음 ppt)
+		// 아무 키 누르면 추출한 컨투어의 shape을 감지한다
 	cout << " Step-2) Press any key to detect shape descriptor...!! " << endl;
 	waitKey(0);
 	
 	image = imread(argv[3], 0);
 	// testing the bounding box
-	Rect r0 = cv::boundingRect(cv::Mat(contours[0]));
-	rectangle(result, r0, cv::Scalar(0), 2);
+	Rect r0 = boundingRect(Mat(contours[0]));
+	rectangle(result, r0, Scalar(0), 2);
+
 	// testing the enclosing circle
 	float radius;
 	Point2f center;
-	minEnclosingCircle(cv::Mat(contours[1]), center, radius);
-	circle(result, cv::Point(center), static_cast<int>(radius), cv::Scalar(0), 2);
+	minEnclosingCircle(Mat(contours[1]), center, radius);
+	circle(result, Point(center), static_cast<int>(radius), Scalar(0), 2);
 	// testing the approximate polygon
-	vector<cv::Point> poly;
-	approxPolyDP(cv::Mat(contours[2]), poly, 5, true);
-	cout << "Polygon size: " << poly.size() << std::endl;
+	vector<Point> poly;
+	approxPolyDP(Mat(contours[2]), poly, 5, true);
+	cout << "Polygon size: " << poly.size() << endl;
 	// Iterate over each segment and draw it
-	vector<cv::Point>::const_iterator itp = poly.begin();
+	vector<Point>::const_iterator itp = poly.begin();
 	while (itp != (poly.end() - 1)) {
-		cv::line(result, *itp, *(itp + 1), cv::Scalar(0), 2);
+		line(result, *itp, *(itp + 1), cv::Scalar(0), 2);
 		++itp;
 	}
 	// last point linked to first point
-	cv::line(result, *(poly.begin()), *(poly.end() - 1), cv::Scalar(20), 2);
+	line(result, *(poly.begin()), *(poly.end() - 1), cv::Scalar(20), 2);
 		
 	// testing the convex hull
-	std::vector<cv::Point> hull;
-	cv::convexHull(cv::Mat(contours[3]), hull);
+	vector<Point> hull;
+	convexHull(Mat(contours[3]), hull);
 	// Iterate over each segment and draw it
-	std::vector<cv::Point>::const_iterator it = hull.begin();
+	vector<Point>::const_iterator it = hull.begin();
 	while (it != (hull.end() - 1)) {
 		cv::line(result, *it, *(it + 1), cv::Scalar(0), 2);
 		++it;
 	}
 	// last point linked to first point
-	cv::line(result, *(hull.begin()), *(hull.end() - 1), cv::Scalar(20), 2);
+	line(result, *(hull.begin()), *(hull.end() - 1), cv::Scalar(20), 2);
 	// 스타트 포인트랑 엔드 포인트를 잇기 위해 마지막에 line을 해준다.
 
 	itc = contours.begin();
+	//moment 로 중심점 잡기 
 	while (itc != contours.end()) {
-		// compute all moments
-		cv::Moments mom = cv::moments(cv::Mat(*itc++));
-		// draw mass center
-		circle(result,Point(mom.m10 / mom.m00, mom.m01 / mom.m00),
-			2, Scalar(0), 2); // draw black dot
+		Moments mom = cv::moments(cv::Mat(*itc++));
+		circle(result,Point(mom.m10 / mom.m00, mom.m01 / mom.m00),2, Scalar(0), 2); 
 	}
 	namedWindow("Some Shape descriptors");
 	imshow("Some Shape descriptors", result);
